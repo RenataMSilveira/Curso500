b1a6087dcf15b514d970b1bda7f89ef76e4c0448
Renata Messias da Silveira <56639937+RenataMSilveira@users.noreply.github.com>
2020-01-20 22:08:20 -0300

    Update info.php

--- info.php
+++ info.php
@@ -1,3 +1,45 @@
 <?php
 
 echo "Hello World!";
+
+
+<?php
+
+echo "Hello World!";
+
+$nome = "Renata";
+$sobrenome = "Messias";
+
+echo $nome . "" . $sobrenome;
+echo "<br>";
+echo $nome;
+echo "<br>";
+$soma = 1 + 1;
+echo $soma;
+
+echo "<br>";
+
+$nome = "Renata";
+$idade = 30;
+
+echo "<br>";
+
+
+$a = 1;
+
+if (1 != 1) {
+    echo "São Iguais";
+} else {
+    echo "Não São Iguais";
+}
+ 
+echo "<br>";
+echo "<br>";
+
+$idade =45 ;
+
+if ( $idade >= 18 ){
+    echo "Maior de Idade";
+} else {
+    echo "Menor de Idade";
+}lstat
