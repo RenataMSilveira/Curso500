727dda3415ac6e63a974bea8f9dec6633e12db23
RENATA <renatamessias.silveira@gmail.com>
2020-01-22 22:21:43 -0300

    Aula4

---
+++ funcoes.php
@@ -0,0 +1,39 @@
+<?php
+
+
+// function soma ($a , $b) {
+//     $resultado = $a + $b;
+//     return "$resultado"
+// }
+
+// function calculapesoideal(string $sexo, float $altura)
+// {
+//     switch ($sexo){
+//         case "M":
+//             return calcula (72.7,58,$altura);
+//         break;
+//         case "F":
+//             return calcula (62.1,44.7,$altura);
+//         break;
+//         default:
+//             return "Sexo invalido!";
+//     }
+// }
+// function calcula($a,$b,$altura) {
+//     return ($a * $altura) - $b;
+// }
+
+function calculaequacao ( )
+{
+
+}
+global $a,$b,$c ;
+
+function calculadelta (float $a,float $b, float $c){
+    return ( pow($b,2) - 4 * $a * $c )
+}
+global $a,$b,$c;
+
+function calculax1 ()
+    return (((-$b) + sqrt(calculadelta)) / 2 * $a)
+}
--- index.php
+++ index.php
@@ -1,166 +1,30 @@
 <?php
 
-// // $op1 = Soma;
-// // $op2 = Divisao;
-// // $op3 = Multiplicacao;
-// // $op4 = Subtracao;
-// // $valor1 = 4;
-// // $valor2 = 15;
+// require ('../html/funcoes.php');
 
-// // $operacao = $op4 ;
+// // echo "$resultado";
 
-// // if ($operacao == $op1){
-// //     echo "$valor1 + $valor2 = ";
-// //     echo ($valor1 + $valor2);
-// // }
-// // if ($operacao == $op2 ){
-// //     echo "$valor1 / $valor2 = ";
-// //     echo ($valor1 / $valor2);
-// // }
-// // if ($operacao == $op3 ){
-// //     echo "$valor1 * $valor2 = ";
-// //     echo ($valor1 * $valor2);
-// // }
-// // if ($operacao == $op4 ){
-// //     echo "$valor1 - $valor2 = ";
-// //     echo ($valor1 - $valor2);
-// // };
+// $sexo = "M";
+// $altura = 1.80;
 
-// // o do Caique
+// echo calculapesoideal($sexo,$altura);
 
-// // $media = 10;
+$a = 2;
+$b = 8;
+$c = 2;
 
-// // switch ($media) {
-// //     case "10":
-// //         echo "Você foi Muito Bem!";
-// //     case "9":
-// //     case "8":
-// //     case ""
-// //         echo "Parabens";
-// //         break;
-// //     case "0":
-// //         echo "Você foi mal";
-// //     break;
-// // }
+function calculadelta (float $a,float $b, float $c){
+    global $a,$b,$c ;
+    echo $resultado = ( pow($b,2) - 4 * $a * $c );
+}
 
-// // $valor1 = 5;
-// // $valor2 = 10;
-// // $operacao = "/";
+function calculax1 (float $a,float $b, float $c){
+    global $a,$b,$c, $resultado;
+    echo $resultado2 = (((-$b) + sqrt($resultado)) / 2 * $a);
+};
 
-// // switch ($operacao){
-// //     case "+":
-// //         echo "$valor1 + $valor2 = ";
-// //         echo ($valor1 + $valor2);
-// //         break;
-// //     case "-":
-// //         echo "$valor1 - $valor2 = ";
-// //         echo ($valor1 - $valor2);
-// //         break;
-// //     case "/":
-// //         echo "$valor1 / $valor2 = ";
-// //         echo ($valor1 / $valor2);
-// //         break;
-// //     case "*":
-// //         echo "$valor1 X $valor2 = ";
-// //         echo ($valor1 * $valor2);
-// //         break;
-// //     default:
-// //         echo "Operação Inválida";
-// //     };
-
-//     // Do Caique
-
-// // $valor1 = 5;
-// // $valor2 = 10;
-// // $operacao = "/";
-
-// // switch ($operacao){
-// //     case "+":
-// //         echo "{$valor1} + {$valor2} = " . ($valor1 + $valor2);
-// //         break;
-// //     case "-":
-// //         echo "{$valor1} - {$valor2} = " . ($valor1 - $valor2);
-// //         break;
-// //     case "/":
-// //         echo "{$valor1} / {$valor2} = " . ($valor1 / $valor2);
-// //         break;
-// //     case "*":
-// //         echo "{$valor1} * {$valor2} = " . ($valor1 * $valor2);
-// //         break;
-// //     default:
-// //         echo "Operação Inválida";
-
-// //     }
-
-// // While
-
-// // $num = 0;
-
-// // while ($num <= 10) {
-// //     echo "$num <br>";
-// //     $num++;
-// // }
-
-// // // Do While
-
-// // $num = 0;
-
-// // do{
-// //     echo "$num <br>";
-// //     $num++;
-// // }
-// // while ($num <= 10) {
-    
-// // }
-// // $num = 0;
-
-// // while ($num <= 10) {
-// //     $num++;
-// //     if ($num === 5){
-// //         continue;
-// //     }
-// //     echo "$num <br>";
+function calcula x2
 
 
-// // }
-
-// // $num1 = 9;
-// // $num2 = 0;
-
-// // while ($num1 &&  $num2 <= 10){
-// //        echo "{$num1} X {$num2}  = " . ($num1 * $num2)."<br> ";
-// //        $num2++;
-// // }
-
-// // for ($par = 0 , $impar = 1; $par <= 100; $par +=2, $impar += 2)
-// //     echo "Par: $par e Impar: $impar <br>" ;     
-// // )
-
-// // *****Meu
-
-// // for ($num1 = 8, $num2 = 0; $num2 <=10; $num1 && $num2++)
-// // {
-// //     echo "$num1 X $num2 = ".($num1 * $num2) . "<br>";
-// // }
-
-// // ****Do Caique
-
-// // $tabuada = 1;
-
-// // for ($i = 0; $i <= 10; $i++) {
-// //     echo " $tabuada * $i = " . ($tabuada * $i) . "<br>";
-// // }
-
-// // *** Como calcular mais de um valor
-
-// for ($i = 1; $i <= 10 && $tabuada <=10; $i++) {
-//         echo " $tabuada * $i = " . ($tabuada * $i) . "<br>";
-        
-//         if($i === 10) {
-//            $i = 0;
-//            $tabuada++;
-//            echo "<br>";
-//         }
-//     }
-
+do Caique
 
