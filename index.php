e3644fd6f3ff65e1b1375f59991478c13748b57c
RENATA <renatamessias.silveira@gmail.com>
2020-01-24 22:27:51 -0300

    Aula

--- index.php
+++ index.php
@@ -1,25 +1,63 @@
 <?php
 
-$numeros = [
-    '4',
-    '16',
-    '80',
-    '57',
-    '44',
-    '100',
-    '33',
-];
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
 
-foreach ($numeros as $numero)
-{
-    for ($numero ;$numero < count($numeros); $numero++ );
-    {
-        if ($numero % 2 === 0){
-            echo ("$numero é Impar <br>");
-        } else { echo ("$numero é Par <br>");
-        }
-    }
-}
+require_once './PHPMailer-6.1.4/src/PHPMailer.php';
+require_once './PHPMailer-6.1.4/src/SMTP.php';
+
+$email = new PHPMailer();
+$email ->setLanguage('pt_br','./lib/PHPMailer-6.1.4/language');
+$email ->isSMTP();
+$email ->Host = 'smtp.gmail.com';
+$email ->Port = 587;
+$email ->SMTPSecure = 'tls';
+$email ->SMTPAuth = true;
+$email ->charSet = PHPMailer :: CHARSET_UTF8;
+$email ->Username = 'becapizzaiola@gmail.com';
+$email ->Password = '123mudar@';
+$email ->setFrom('becapizzaiola@gmail.com');
+$email ->addAddress ('caiqueportela@4linux.com.br');
+$email ->Subject = 'Teste no Curso de PHP';
+$email ->isHTML (true);
+$email ->Body = '<h1>Enviei com o PHP!</h1>';
+
+if (!$email ->send()){
+    print("erro: {$email->ErrorInfo}");
+} else { 
+    print("E-mail Enviado!");
+};
+
+?>
+// <!-- <!-- <form method= "post">
+//     <label>Usuario: </label> 
+    <imput type="text" name="usuario">
+    <br>
+    <label>Senha: </label> 
+    <imput type="text" name="senha">
+    <br>
+    <button type="submit">Entrar</button
+
+// $numeros = [ -->
+//     '4',
+//     '16',
+//     '80',
+//     '57',
+//     '44',
+//     '100',
+//     '33',
+// ];
+
+// foreach ($numeros as $numero)
+// {
+//     for ($numero ;$numero < count($numeros); $numero++ );
+//     {
+//         if ($numero % 2 === 0){
+//             echo ("$numero é Impar <br>");
+//         } else { echo ("$numero é Par <br>");
+//         }
+//     }
+// }
 
 
 
@@ -73,4 +111,4 @@ foreach ($numeros as $numero)
 // foreach ($cursos as $curso => $alunos) {
 //     list($posicao1, $posicao2) = $alunos;
 //     print("Curso $curso possui os alunos $posicao1 e $posicao2 <br>");
-// }
+// } -->
